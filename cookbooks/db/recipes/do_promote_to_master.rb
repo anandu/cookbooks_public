faeda42e40912f0993a52e0677726fa8a5367f4c
Your Name <anandu@gmail.com>
2011-12-08 01:32:27 +0530

    adding new recipes

--- cookbooks/db/recipes/do_promote_to_master.rb
+++ cookbooks/db/recipes/do_promote_to_master.rb
@@ -1,25 +1,9 @@
+#
 # Cookbook Name:: db
 #
-# Copyright (c) 2011 RightScale Inc
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
 rs_utils_marker :begin
 
@@ -30,11 +14,10 @@ db_state_assert :slave
 
 # == Open port for slave replication by old-master
 #
-sys_firewall "Open port 3306 to the old master which is becoming a slave" do
-  port 3306
+db node[:db][:data_dir] do
   enable true
-  ip_addr node[:db][:current_master_ip]
-  action :update
+  ip_addr node[:cloud][:private_ips][0]
+  action :firewall_update_request
 end
 
 # == Promote to master
