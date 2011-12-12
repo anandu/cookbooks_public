ae195afa22e1bb0a20d2a2bf1b5cd78f8ab6756f
Anand <anandu@gmail.com>
2011-12-12 23:40:33 +0530

    enabled "backup_lock_take"

--- cookbooks/db/definitions/db_do_backup.rb
+++ cookbooks/db/definitions/db_do_backup.rb
@@ -71,10 +71,10 @@ define :db_do_backup, :force => false, :backup_type => "primary" do
   # See below for more information about 'backup.rb'
   # if 'force' is true, kills pid and removes locks
   #
-  #block_device DATA_DIR do
-  #  action :backup_lock_take
-  #  force do_force
-  #end
+  block_device DATA_DIR do
+    action :backup_lock_take
+    force do_force
+  end
   
   #log "  Performing (#{do_backup_type} backup) lock DB and write backup info file..."
   db DATA_DIR do
