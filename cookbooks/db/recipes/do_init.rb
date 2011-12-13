a552f5e92b5fc7cfeb7fbeaf0de83bf5fec64f32
Anand <anandu@gmail.com>
2011-12-13 21:20:25 +0530

    Added all init functions

--- cookbooks/db/recipes/do_init.rb
+++ cookbooks/db/recipes/do_init.rb
@@ -16,36 +16,37 @@ db_init_status :check do
 end
 
 log "  Stopping database..."
-#db DATA_DIR do
-#  action :stop
-#end
+db DATA_DIR do
+  action :stop
+end
 
-#log "  Creating block device..."
-#block_device DATA_DIR do
-#  lineage node[:db][:backup][:lineage]
-#  action :create
-#end
+log "  Creating block device..."
+block_device DATA_DIR do
+  lineage node[:db][:backup][:lineage]
+  action :create
+end
 
-#log "  Moving database to block device and starting database..."
-#db DATA_DIR do
-#  action [ :move_data_dir, :start ]
-#end
+log "  Moving database to block device and starting database..."
+db DATA_DIR do
+  action [ :move_data_dir, :start ]
+end
 
 log "  Setting state of database to be 'initialized'..."
 db_init_status :set
 
-log "  Registering as master..."
+# Following feature are removed from the recipe to enable it for stand-alone mode
+#log "  Registering as master..."
 #db_register_master
 
-log "  Adding replication privileges for this master database..."
+#log "  Adding replication privileges for this master database..."
 #include_recipe "db::setup_replication_privileges"
 
-log "  Forcing a backup so slaves can init from this master..."
+#log "  Forcing a backup so slaves can init from this master..."
 #db_do_backup "do force backup" do
 #  force true
 #end
 
-log "  Setting up cron to do scheduled backups..."
+#log "  Setting up cron to do scheduled backups..."
 #include_recipe "db::do_backup_schedule_enable"
 
 rs_utils_marker :end
